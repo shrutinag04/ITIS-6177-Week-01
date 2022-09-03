6d18aac7bb7759bb8671ff3f9db8d72cf9a62968
Shruti Nagpure <shrutinagpure@Shrutis-MacBook-Air.local>
2022-09-03 16:53:44 -0400

    simple node server

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
-
+var http= require('http')
+http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Hello World\n');
+ }).listen(8080);
 console.log('Hello World');

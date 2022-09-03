18d7aa8d22ea43cfa8bdfcada1f658fe869b17ed
Shruti Nagpure <shrutinagpure@Shrutis-MacBook-Air.local>
2022-09-03 17:17:07 -0400

    created nodejs server using express

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-var http= require('http')
-http.createServer(function (request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Hello World\n');
- }).listen(8080);
-console.log('Hello World');
+const express = require('express')
+const app = express()
+const port = 8080
+
+app.get('/', (req,res) => {
+    res.send('Hello World')
+})
+
+app.listen(port, ()=>{
+    console.log(`Server running on port ${port}`)
+})
